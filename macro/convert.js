805d94aa530ee2b931a3228c63cffb719141ee2e
Todd Wolfson <todd@twolfson.com>
2012-09-29 15:28:46 -0700

    Writing out remaining cases to handle

--- macro/convert.js
+++ macro/convert.js
@@ -10,11 +10,41 @@ jsminc = jsminc.replace(/static int\s+(the[^\n]+)/g, function (_, varStr) {
 });
 
 // Throw in an EOF before the first theA
-jsminc = jsminc.replace('var theA;', 'var EOF = {};\nvar theA;');
+jsminc = jsminc.replace('var theA;', 'var EOF = -1;\nvar theA;');
 
 // Replace static void and static int with function
 jsminc = jsminc.replace(/static void[^A-Za-z]*/g, 'function ');
 jsminc = jsminc.replace(/static int[^A-Za-z]*/g, 'function ');
 jsminc = jsminc.replace(/extern int[^A-Za-z]*/g, 'function ');
 
+// Remove types from function statements
+jsminc = jsminc.replace(/function ([^\(]+)\(([^\)]+)\)[^{]+/g, function (_, fnName, params) {
+  var paramArr = params.split(', '),
+      cleanParamArr = paramArr.map(function (param) {
+        var paramSansFront = param.replace(/^[^\s]+\s+/, ''),
+            paramSansBack = paramSansFront.replace('[]', '');
+        return paramSansBack;
+      }),
+      cleanParams = cleanParamArr.join(', ');
+
+  return 'function ' + fnName + '(' + cleanParams + ') ';
+});
+
+// Replace types inside functions with vars
+jsminc = jsminc.replace(/int /g, 'var ');
+
+// TODO: Strict equality
+// TODO: Module.exports
+
+// This should be fputs -> concatenates onto an error then exit(1) throws concatenated piece
+// TODO: Handle fputs("JSMIN Error: ", stderr);
+// TODO: Handle fputc('\n', stderr);
+// TODO: Handle exit(1);
+
+// TODO: Handle c = getc(stdin);
+// TODO: Handle putc(theA, stdout);
+
+// TODO: Handle fprintf(stdout, "// %s\n", argv[i]);
+
+// Write out the converted file
 fs.writeFileSync(__dirname + '/../lib/jsmin.c.js', jsminc, 'utf8');

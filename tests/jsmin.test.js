6fa803c43b2fef39e9805c6357db2ee5c93aeed1
Todd Wolfson <todd@twolfson.com>
2012-09-29 18:31:40 -0700

    Moved to new JSMin API

--- tests/jsmin.test.js
+++ tests/jsmin.test.js
@@ -5,10 +5,11 @@ var fs = require('fs'),
     expectedDir = __dirname + '/expected_files',
     jQuerySrc = fs.readFileSync(testFilesDir + '/jquery.js', 'utf8'),
     expectedJQuery = fs.readFileSync(expectedDir + '/jquery.min.js', 'utf8'),
-    actualJQuery = jsmin(jQuerySrc);
+    actualJQuery = jsmin(jQuerySrc),
+    actualJQueryCode = actualJQuery.code;
 
 // Assert that the minified jQuery matches the expected version
-assert.strictEqual(expectedJQuery, actualJQuery, 'Minified jQuery does not match as expected.');
+assert.strictEqual(expectedJQuery, actualJQueryCode, 'Minified jQuery does not match as expected.');
 
 // Log success when done
 console.log('Success!');

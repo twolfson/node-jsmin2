bb7dde3f649a2b4ac15a84345e8a4fc4ff038d31
Todd Wolfson <todd@twolfson.com>
2012-10-07 18:57:01 -0700

    Added codeMap test

--- tests/jsmin.test.js
+++ tests/jsmin.test.js
@@ -6,10 +6,37 @@ var fs = require('fs'),
     jQuerySrc = fs.readFileSync(testFilesDir + '/jquery.js', 'utf8'),
     expectedJQuery = fs.readFileSync(expectedDir + '/jquery.min.js', 'utf8'),
     actualJQuery = jsmin(jQuerySrc),
-    actualJQueryCode = actualJQuery.code;
+    actualJQueryCode = actualJQuery.code,
+    actualJQueryMap = actualJQuery.codeMap;
 
 // Assert that the minified jQuery matches the expected version
 assert.strictEqual(expectedJQuery, actualJQueryCode, 'Minified jQuery does not match as expected.');
+
+// Reverse the map for checking
+var codeMap = {};
+Object.getOwnPropertyNames(actualJQueryMap).forEach(function (srcPoint) {
+  var destPoint = actualJQueryMap[srcPoint];
+  codeMap[destPoint] = srcPoint;
+});
+
+// Assert that the mapping is proper
+var i = 0,
+    len = actualJQueryCode.length,
+    srcIndex,
+    char,
+    srcChar;
+for (; i < len; i++) {
+  char = actualJQueryCode.charAt(i);
+  srcIndex = codeMap[i];
+  srcChar = jQuerySrc.charAt(srcIndex);
+
+  // Debug here
+  // console.log(char, srcChar);
+
+  // Assert that the char and srcChar line up
+  assert.strictEqual(char, srcChar, 'Character at ' + i + ' does not match source character at ' + srcIndex + '.');
+}
+
 
 // Log success when done
 console.log('Success!');

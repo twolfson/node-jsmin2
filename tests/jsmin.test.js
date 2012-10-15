0ed5674ee6731ea0b0daa4247a64cbd7d3cda7d8
Todd Wolfson <todd@twolfson.com>
2012-10-14 18:06:23 -0700

    Added extra logic for handling mapping test of comment-less head

--- tests/jsmin.test.js
+++ tests/jsmin.test.js
@@ -5,38 +5,37 @@ var fs = require('fs'),
     expectedDir = __dirname + '/expected_files',
     jQuerySrc = fs.readFileSync(testFilesDir + '/jquery.js', 'utf8'),
     expectedJQuery = fs.readFileSync(expectedDir + '/jquery.min.js', 'utf8'),
-    // actualJQuery = jsmin(jQuerySrc),
-    actualJQuery = {},
+    actualJQuery = jsmin(jQuerySrc),
     actualJQueryCode = actualJQuery.code,
     actualJQueryMap = actualJQuery.codeMap;
 
-// // Assert that the minified jQuery matches the expected version
-// assert.strictEqual(expectedJQuery, actualJQueryCode, 'Minified jQuery does not match as expected.');
+// Assert that the minified jQuery matches the expected version
+assert.strictEqual(expectedJQuery, actualJQueryCode, 'Minified jQuery does not match as expected.');
 
-// // Reverse the map for checking
-// var codeMap = {};
-// Object.getOwnPropertyNames(actualJQueryMap).forEach(function (srcPoint) {
-//   var destPoint = actualJQueryMap[srcPoint];
-//   codeMap[destPoint] = srcPoint;
-// });
+// Reverse the map for checking
+var codeMap = {};
+Object.getOwnPropertyNames(actualJQueryMap).forEach(function (srcPoint) {
+  var destPoint = actualJQueryMap[srcPoint];
+  codeMap[destPoint] = srcPoint;
+});
 
-// // Assert that the mapping is proper
-// var i = 0,
-//     len = actualJQueryCode.length,
-//     srcIndex,
-//     char,
-//     srcChar;
-// for (; i < len; i++) {
-//   char = actualJQueryCode.charAt(i);
-//   srcIndex = codeMap[i];
-//   srcChar = jQuerySrc.charAt(srcIndex);
+// Assert that the mapping is proper
+var i = 0,
+    len = actualJQueryCode.length,
+    srcIndex,
+    char,
+    srcChar;
+for (; i < len; i++) {
+  char = actualJQueryCode.charAt(i);
+  srcIndex = codeMap[i];
+  srcChar = jQuerySrc.charAt(srcIndex);
 
-//   // Debug here
-//   // console.log(char, srcChar);
+  // Debug here
+  // console.log(char, srcChar);
 
-//   // Assert that the char and srcChar line up
-//   assert.strictEqual(char, srcChar, 'Character at ' + i + ' does not match source character at ' + srcIndex + '.');
-// }
+  // Assert that the char and srcChar line up
+  assert.strictEqual(char, srcChar, 'Character at ' + i + ' does not match source character at ' + srcIndex + '.');
+}
 
 // Assert that basic matches as expected
 var basicSrc = fs.readFileSync(testFilesDir + '/basic.js', 'utf8'),
@@ -46,6 +45,40 @@ var basicSrc = fs.readFileSync(testFilesDir + '/basic.js', 'utf8'),
     actualBasicMap = actualBasic.codeMap;
 
 assert.strictEqual(expectedBasic, actualBasicCode, 'Minified basic does not match as expected.');
+
+// Reverse the map for checking
+var codeMap = {};
+Object.getOwnPropertyNames(actualBasicMap).forEach(function (srcPoint) {
+  var destPoint = actualBasicMap[srcPoint];
+  codeMap[destPoint] = srcPoint;
+});
+
+// Assert that the mapping is proper
+var i = 0,
+    len = actualBasicCode.length,
+    srcIndex,
+    char,
+    srcChar,
+    timesSkipped = 0;
+for (; i < len; i++) {
+  char = actualBasicCode.charAt(i);
+  srcIndex = codeMap[i];
+
+  if (srcIndex !== undefined) {
+    srcChar = basicSrc.charAt(srcIndex);
+
+    // Debug here
+    // console.log(char, srcChar);
+
+    // Assert that the char and srcChar line up
+    assert.strictEqual(char, srcChar, 'Character at ' + i + ' does not match source character at ' + srcIndex + '.');
+  } else {
+    timesSkipped += 1;
+  }
+}
+
+// Assert that during the mapping we skipped at most once
+assert.ok(timesSkipped <= 1, 'During mapping, we skipped at most once');
 
 // Log success when done
 console.log('Success!');
